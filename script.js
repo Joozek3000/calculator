66930ebb76676cfacacb763aa77b9cd176297e64
daedalus <j.jozefczyk90@gmail.com>
2022-09-10 16:42:45 +0100

    added function and number buttons functionaloties

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ const operationsBtn = document.querySelectorAll('[data-operations]');
 const equalsBtn = document.querySelector('[data-equals]');
 const clearBtn = document.querySelector('[data-all-clear]');
 const deleteBtn = document.querySelector('[data-delete]');
+const signBtn = document.querySelector('[data-sign]');
 const previousOperand = document.querySelector('[data-previous-operand]');
 const currentOperand = document.querySelector('[data-current-operand]');
+const fuctionBtn = document.querySelectorAll('.function-button');
+
+numbersBtn.forEach((number) => {
+  number.dataset.number = number.textContent;
+});
 
 // displaying numbers
 numbersBtn.forEach((button) => {
   button.addEventListener('click', (e) => {
-    currentOperand.textContent === '0'
+    let displayStr = currentOperand.textContent;
+    displayStr === '0'
       ? (currentOperand.textContent = e.target.textContent)
       : (currentOperand.textContent += e.target.textContent);
+    // check if decimal point is already pressed
+    if (displayStr.includes('.') === true) {
+      document.querySelector('.dot').disabled = true;
+    } else {
+      document.querySelector('.dot').disabled = false;
+    }
   });
+});
+
+// function buttons
+fuctionBtn.forEach((button) => {
+  button.addEventListener('click', (e) => {});
 });
 
 // clear button
@@ -22,6 +40,33 @@ clearBtn.addEventListener('click', () => {
     currentOperand.textContent = '0';
   };
   return clear();
+});
+
+// delete button
+deleteBtn.addEventListener('click', () => {
+  if (currentOperand.textContent !== '0') {
+    currentOperand.textContent = currentOperand.textContent.substring(
+      0,
+      currentOperand.textContent.length - 1
+    );
+  }
+});
+
+// sign
+signBtn.addEventListener('click', () => {
+  if (currentOperand.textContent === '0') {
+    currentOperand.textContent = '-';
+  } else {
+    let displayArr = currentOperand.textContent.split('');
+    // if display number is negative, delete '-'
+    if (displayArr[0] === '-') {
+      displayArr.shift();
+      currentOperand.textContent = displayArr.join('');
+    } else {
+      displayArr.unshift('-');
+      currentOperand.textContent = displayArr.join('');
+    }
+  }
 });
 
 // addition
